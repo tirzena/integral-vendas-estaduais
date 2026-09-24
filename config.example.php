231fdb64b13052e707b8c6698f10208c686b1810
tirzena <tirzenasistema@gmail.com>
2026-09-24 17:52:22 -0400

    Adicionar config.example.php para teste PHP de vendas-estaduais

---
+++ config.example.php
@@ -0,0 +1,2 @@
+<?php
+return ['db'=>['dsn'=>'mysql:host=localhost;dbname=BASE_DA_DIVISAO;charset=utf8mb4','user'=>'','password'=>''],'system_code'=>'vendas_estaduais','central_api'=>'https://teste.qrcodevalidacao.com/api.php','service_key'=>'CHAVE_INDIVIDUAL_COM_32_CARACTERES_OU_MAIS'];
